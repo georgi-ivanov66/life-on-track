bd734f8dcc462a964bf77bb4efcf6d344f87892a
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2022-03-06 13:27:10 +0000

    added error handling to signup form

--- src/views/SignupForm.vue
+++ src/views/SignupForm.vue
@@ -17,7 +17,13 @@
             ></ion-icon
             >Name:</ion-label
           >
-          <ion-input type="text" required v-model="enteredName"> </ion-input>
+          <ion-input
+            type="text"
+            required
+            v-model="enteredName"
+            @ionFocus="clearErrors"
+          >
+          </ion-input>
         </ion-item>
         <ion-item>
           <ion-label position="floating"
@@ -25,8 +31,13 @@
             </ion-icon>
             Email:
           </ion-label>
-
-          <ion-input type="email" required v-model="enteredEmail"> </ion-input>
+          <ion-input
+            type="email"
+            required
+            v-model="enteredEmail"
+            @ionFocus="clearErrors"
+          >
+          </ion-input>
         </ion-item>
         <ion-item>
           <ion-label position="floating">
@@ -38,10 +49,24 @@
             </ion-icon
             >Password:</ion-label
           >
-          <ion-input type="password" required v-model="enteredPassword">
+          <ion-input
+            type="password"
+            required
+            v-model="enteredPassword"
+            @ionFocus="clearErrors"
+          >
           </ion-input>
         </ion-item>
       </ion-list>
+      <ion-toast
+        v-if="errorThrown"
+        animated
+        header="Could not sign you up"
+        :message="errorMessage"
+        color="danger"
+        position="top"
+        :icon="icons.alertOutline"
+      ></ion-toast>
       <ion-button
         style="margin: 20px"
         shape="round"
@@ -63,10 +88,22 @@ import {
   IonButton,
   IonBackButton,
   IonIcon,
+  IonToast,
+  toastController,
 } from "@ionic/vue";
-import { personOutline, mailOutline, lockClosedOutline } from "ionicons/icons";
+import {
+  personOutline,
+  mailOutline,
+  lockClosedOutline,
+  alertOutline,
+} from "ionicons/icons";
 import { defineComponent } from "@vue/runtime-core";
-import { getAuth, createUserWithEmailAndPassword, User } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  User,
+  AuthError,
+} from "firebase/auth";
 import { getFirestore, setDoc, doc } from "firebase/firestore";
 export default defineComponent({
   name: "SignupPage",
@@ -78,6 +115,7 @@ export default defineComponent({
     IonButton,
     IonBackButton,
     IonIcon,
+    IonToast,
   },
   data() {
     return {
@@ -88,7 +126,20 @@ export default defineComponent({
         personOutline,
         mailOutline,
         lockClosedOutline,
+        alertOutline,
       },
+      errorThrown: false,
+      errorMessage: "",
+      errors: [
+        {
+          code: "auth/email-already-in-use",
+          message: "A user already exists with the provided email.",
+        },
+        {
+          code: "auth/weak-password",
+          message: "Your password does not meet the requirements",
+        },
+      ],
     };
   },
   methods: {
@@ -104,12 +155,18 @@ export default defineComponent({
           this.addUserToFirestore(value.user);
           this.$router.push("/Dashboard");
         })
-        .catch((error) => {
-          console.error(error);
+        .catch((error: AuthError) => {
+          const errorMatch = this.errors.find((e) => e.code === error.code);
+          this.errorThrown = true;
+          this.errorMessage = errorMatch
+            ? errorMatch.message
+            : "An error has occurred, please try again";
         });
       this.enteredPassword = "";
       this.enteredEmail = "";
+      this.enteredName = "";
     },
+
     async addUserToFirestore(user: User) {
       try {
         const db = getFirestore();
@@ -120,6 +177,15 @@ export default defineComponent({
       } catch (e) {
         console.error("Error adding document: ", e);
       }
+    },
+
+    async clearErrors() {
+      const toast = await toastController.getTop();
+      if (toast) {
+        await toastController.dismiss();
+      }
+      this.errorThrown = false;
+      this.errorMessage = "";
     },
   },
 });
